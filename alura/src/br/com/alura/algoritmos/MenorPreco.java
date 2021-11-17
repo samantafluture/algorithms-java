85ec2cec0b17997d1beed4ad611500538b0a6c10
Samanta Fluture <samantafluture@gmail.com>
2021-11-17 15:31:45 -0200

    create algo to search for the most expensive item

--- alura/src/br/com/alura/algoritmos/MenorPreco.java
+++ alura/src/br/com/alura/algoritmos/MenorPreco.java
@@ -2,18 +2,17 @@ package br.com.alura.algoritmos;
 
 public class MenorPreco {
     public static void main(String[] args) {
-        Produto produtos[] = { 
-            new Produto("Lamborghini", 1000000), 
-            new Produto("Jipe", 46000),
-            new Produto("Brasília", 16000), 
-            new Produto("Smart", 46000), 
-            new Produto("Fusca", 17000) 
-        };
+        Produto produtos[] = { new Produto("Lamborghini", 1000000), new Produto("Jipe", 46000),
+                new Produto("Brasília", 16000), new Produto("Smart", 46000), new Produto("Fusca", 17000) };
 
         int maisBarato = buscaMenor(produtos);
+        int maisCaro = buscaMaior(produtos);
 
         System.out.println("O carro " + produtos[maisBarato].getNome() + " é o mais barato, e custa R$ "
                 + produtos[maisBarato].getPreco());
+
+        System.out.println("O carro " + produtos[maisCaro].getNome() + " é o mais caro, e custa R$ "
+                + produtos[maisCaro].getPreco());
     }
 
     private static int buscaMenor(Produto[] produtos) {
@@ -26,5 +25,17 @@ public class MenorPreco {
         }
 
         return maisBarato;
+    }
+
+    private static int buscaMaior(Produto[] produtos) {
+        int maisCaro = 0;
+
+        for (int atual = 0; atual < produtos.length; atual++) {
+            if (produtos[atual].getPreco() > produtos[maisCaro].getPreco()) {
+                maisCaro = atual;
+            }
+        }
+
+        return maisCaro;
     }
 }
